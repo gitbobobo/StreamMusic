f785833c494663312ab842f3e3cc8b38adc8468a
gitbobobo <godbobo@aliyun.com>
2024-03-16 21:35:08 +0800

    修正编辑链接

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/gitbobobo/StreamMusic/blob/main/',
             async sidebarItemsGenerator({defaultSidebarItemsGenerator, ...args}) {
               const sidebarItems = await defaultSidebarItemsGenerator(args);
               return reverseSidebarItems(sidebarItems);
